2b5759aa66e0c74d6bce427b47e079b1ed2f3f14
My PC <alex.petkov606@gmail.com>
2023-03-14 17:49:30 +0200

    asd

---
+++ demo/src/main/java/com/example/demo/models/Category.java
@@ -0,0 +1,24 @@
+package com.example.demo.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category extends BaseEntity {
+
+    private String name;
+
+
+    public Category() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
