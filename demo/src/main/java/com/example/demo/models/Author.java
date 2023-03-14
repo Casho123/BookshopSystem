b46cb6808c7f1ee9b765bb70f57cd384c84fe27e
My PC <alex.petkov606@gmail.com>
2023-03-14 17:48:01 +0200

    asd

--- demo/src/main/java/com/example/demo/models/Author.java
+++ demo/src/main/java/com/example/demo/models/Author.java
@@ -1,9 +1,34 @@
 package com.example.demo.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "authors")
-public class Author {
+public class Author extends BaseEntity {
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    public Author() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
