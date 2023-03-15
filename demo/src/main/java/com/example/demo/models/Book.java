7c294708be977a735a0665eda75b805e99cdfdac
My PC <alex.petkov606@gmail.com>
2023-03-15 19:21:30 +0200

    asd

--- demo/src/main/java/com/example/demo/models/Book.java
+++ demo/src/main/java/com/example/demo/models/Book.java
@@ -1,19 +1,108 @@
 package com.example.demo.models;
 
+import com.example.demo.models.enums.AgeRestriction;
 import com.example.demo.models.enums.EditionType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Set;
 
 @Entity
 @Table(name = "books")
 public class Book extends BaseEntity {
 
+    @Column(length = 50, nullable = false)
     private String title;
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String description;
+    @Enumerated
     private EditionType editionType;
+    @Column(nullable = false)
     private BigDecimal price;
+    private Integer copies;
+    @Column(name = "release_date")
+    private Date releaseDate;
+    @Enumerated
+    private AgeRestriction ageRestriction;
+    @ManyToOne
+    private Author author;
+    @ManyToMany
+    private Set<Category> categories;
 
 
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public EditionType getEditionType() {
+        return editionType;
+    }
+
+    public void setEditionType(EditionType editionType) {
+        this.editionType = editionType;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getCopies() {
+        return copies;
+    }
+
+    public void setCopies(Integer copies) {
+        this.copies = copies;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public AgeRestriction getAgeRestriction() {
+        return ageRestriction;
+    }
+
+    public void setAgeRestriction(AgeRestriction ageRestriction) {
+        this.ageRestriction = ageRestriction;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 }
