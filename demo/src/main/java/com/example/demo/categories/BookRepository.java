f719f44160cf07b00594a115e9f23d9ed3948688
My PC <alex.petkov606@gmail.com>
2023-03-16 13:37:57 +0200

    asd

--- demo/src/main/java/com/example/demo/categories/BookRepository.java
+++ demo/src/main/java/com/example/demo/categories/BookRepository.java
@@ -4,6 +4,11 @@ import com.example.demo.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findAllByReleaseDateAfter(LocalDate releaseDateAfter);
 }
