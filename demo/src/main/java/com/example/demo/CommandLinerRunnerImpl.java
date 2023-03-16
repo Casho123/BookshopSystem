1caa5ca1d269bedf63363bafee943c827f2be398
My PC <alex.petkov606@gmail.com>
2023-03-16 12:26:36 +0200

    asd

--- demo/src/main/java/com/example/demo/CommandLinerRunnerImpl.java
+++ demo/src/main/java/com/example/demo/CommandLinerRunnerImpl.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.service.CategoryServiceImpl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommandLinerRunnerImpl implements CommandLineRunner {
 
+
+    private final CategoryServiceImpl categoryService;
+
+    public CommandLinerRunnerImpl(CategoryServiceImpl categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @Override
     public void run(String... args) throws Exception {
+
+        this.categoryService.seedCategories();
+
+
 
     }
 }
--- demo/src/main/java/com/example/demo/service/CategoryServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/CategoryServiceImpl.java
@@ -1,7 +1,37 @@
 package com.example.demo.service;
 
+import com.example.demo.categories.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+
+    private static final String CATEGORIES_FILE_PATH = "C:\\Users\\My PC\\Desktop\\demo (1)\\demo\\src\\main\\resources\\files\\categories.txt";
+
+
+    private final CategoryRepository categoryRepository;
+
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+
+    @Override
+    public void seedCategories() throws IOException {
+        List<String> rows = Files.readAllLines(Path.of(CATEGORIES_FILE_PATH))
+                .stream()
+                .filter(row -> !row.isEmpty())
+                .collect(Collectors.toList());
+
+
+
+    }
 }
+
