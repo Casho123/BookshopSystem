2da5b87f0d9996e6a2da9b43c4873da0f1e77c5e
My PC <alex.petkov606@gmail.com>
2023-03-17 20:38:44 +0200

    asd

--- demo/src/main/java/com/example/demo/configuration/ApplicationBeanConfiguration.java
+++ demo/src/main/java/com/example/demo/configuration/ApplicationBeanConfiguration.java
@@ -2,7 +2,16 @@ package com.example.demo.configuration;
 
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 @Configurable
 public class ApplicationBeanConfiguration {
+
+    @Bean
+    public BufferedReader bufferedReader() {
+        return new BufferedReader(new InputStreamReader(System.in));
+    }
 }
